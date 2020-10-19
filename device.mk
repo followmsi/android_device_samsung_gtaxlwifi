3f020288efaf506798b41eeb84728baa28609d6f
TALU <taluemail272@gmail.com>
2020-10-19 19:23:34 +0100

    gtaxl: upgrade the audio HAL to version 6.

--- device.mk
+++ device.mk
@@ -201,12 +201,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@5.0 \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
